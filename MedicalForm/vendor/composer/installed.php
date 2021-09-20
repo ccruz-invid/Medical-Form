15a471775694cf27d6257684e24e68410eb22f2e
ccruz-invid <ccruz@invidgroup.com>
2021-09-20 10:41:19 -0400

    MedicalForm-Branch

---
+++ MedicalForm/vendor/composer/installed.php
@@ -0,0 +1,40 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-MedicalForm',
+    'version' => 'dev-MedicalForm',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'edbc8bbb9665d7eac0baab0b5dd4f35d316a93b6',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-MedicalForm',
+      'version' => 'dev-MedicalForm',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'edbc8bbb9665d7eac0baab0b5dd4f35d316a93b6',
+    ),
+    'twbs/bootstrap' => 
+    array (
+      'pretty_version' => 'v4.0.0',
+      'version' => '4.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8a628b943cf31ca0a002c08af661a95772480225',
+    ),
+    'twitter/bootstrap' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v4.0.0',
+      ),
+    ),
+  ),
+);
