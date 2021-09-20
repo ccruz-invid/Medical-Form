df853fbfcdd1bdb46cb4519fa8271bf76ce2b055
ccruz-invid <ccruz@invidgroup.com>
2021-09-20 17:38:06 -0400

    NEWMedicalForm-Branch1

--- MedicalForm/src/app/app.module.ts
+++ MedicalForm/src/app/app.module.ts
@@ -5,17 +5,22 @@ import { MatSliderModule } from '@angular/material/slider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { NewMedicalFormComponent } from './modules/new-medical-form/new-medical-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NewMedicalFormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    FormsModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ MedicalForm/src/app/modules/new-medical-form/new-medical-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-medical-form',
+  templateUrl: './new-medical-form.component.html',
+  styleUrls: ['./new-medical-form.component.css']
+})
+export class NewMedicalFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
