dc733651a55a9d67cae9c372ecb0dec856822bda
ccruz-invid <ccruz@invidgroup.com>
2021-09-20 13:54:04 -0400

    MedicalForm-Branch2

---
+++ MedicalForm/src/app/modules/patient/patient.service.ts
@@ -0,0 +1,18 @@
+export interface IPatient{
+    PatientID: string;
+    FirstName: string;
+    LastName: string;
+    SecondLastName: string;
+    MiddleName: string;
+    //DateOfBirth: Date/Time;
+    Age: number;
+    Gender: String;
+    Pregnant: string;
+    GestationTime: number;
+    Address: String;
+    City: String;
+    State: String;
+    ZipCode: number;
+    Phone: Number;
+    SSN: number;
+}
