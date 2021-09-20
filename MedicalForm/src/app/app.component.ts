4879a661046f2fa29e34e5017eec4a1106b0e656
ccruz-invid <ccruz@invidgroup.com>
2021-09-20 16:42:23 -0400

    MedicalForm-Branch4

--- MedicalForm/src/app/app.component.ts
+++ MedicalForm/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'pm-root',
+  template: `
+  <div><h1>{{pageTitle}}</h1>
+  </div>
+   `
 })
 export class AppComponent {
-  title = 'MedicalForm';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  pageTitle: string = 'Medical Form';
 }
--- MedicalForm/src/app/app.module.ts
+++ MedicalForm/src/app/app.module.ts
@@ -5,20 +5,17 @@ import { MatSliderModule } from '@angular/material/slider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PatientListComponent } from './modules/patient/patient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule,
-    PatientListComponent
+    MatSliderModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
